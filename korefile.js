18f069f01cd89d81a360ddd7724ff087d78292a9
John Girvin <hello@nivrig.com>
2018-05-31 16:26:12 +0100

    change include paths to help IDEs without recursive includes

--- korefile.js
+++ korefile.js
@@ -7,7 +7,7 @@ project.addIncludeDir('Sources/KoolBox/Camera');
 project.addIncludeDir('Sources/KoolBox/Core');
 project.addIncludeDir('Sources/KoolBox/Graphics');
 project.addIncludeDir('Sources/KoolBox/Vendor');
-project.addIncludeDir('Sources/Scenes/**');
+project.addIncludeDir('Sources/Scenes/');
 project.setDebugDir('Deployment');
 
 resolve(project);
